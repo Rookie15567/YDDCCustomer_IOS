9339262a4dc45fe6e112d2610e2a4aaba3da1cf4
wangxx <wangxx@wangxinxindeMacBook-Pro.local>
2015-08-27 17:50:26 +0800

    Initial Commit

---
+++ YDDCCustomer_IOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YDDCCustomer_IOS
+//
+//  Created by wangxx on 15/8/27.
+//  Copyright (c) 2015年 CDS_CZC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YDDCCustomer_IOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YDDCCustomer_IOS
+//
+//  Created by wangxx on 15/8/27.
+//  Copyright (c) 2015年 CDS_CZC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
